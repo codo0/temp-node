2819c2807372fa2b9ea42cc3fd818a7bfeb59fc9
codo0 <mohamedlmhamdi13@gmail.com>
2024-01-13 20:58:07 +0100

    commit done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const _ = require("lodash");
-const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 0];
+const items = [1, 2, 3, [4, 5], 6, [7, [8]], 9, 9, 0];
 
-const newItems = _.partition(items, (n) => n % 2);
+const newItems = _.flattenDeep(items);
 
 console.log(newItems);
